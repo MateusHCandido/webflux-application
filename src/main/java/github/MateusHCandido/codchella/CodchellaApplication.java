a4eac32cd9240242c866d2c3fbaa6d0d1006d827
Mateus Henrique <mateushcandido5@gmail.com>
2025-11-02 22:10:29 -0300

    initial project

---
+++ src/main/java/github/MateusHCandido/codchella/CodchellaApplication.java
@@ -0,0 +1,13 @@
+package github.MateusHCandido.codchella;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodchellaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodchellaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/github/MateusHCandido/codchella/CodchellaApplicationTests.java
@@ -0,0 +1,13 @@
+package github.MateusHCandido.codchella;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodchellaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
